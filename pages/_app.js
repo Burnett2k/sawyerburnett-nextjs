69073d19ed1a40954ecbb4a0435bb13f81bce007
Sawyer Burnett <sawyerburnett@gmail.com>
2019-05-19 14:42:14 -0700

    update page title

--- pages/_app.js
+++ pages/_app.js
@@ -41,7 +41,7 @@ class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>My page</title>
+          <title>Sawyer Burnett</title>
         </Head>
         {/* Wrap every page in Jss and Theme providers */}
         <JssProvider
