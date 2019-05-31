87da933cb09c4ed54a865161ac9012358161565a
Sawyer Burnett <sawyerburnett@gmail.com>
2019-05-31 10:41:01 -0500

    updating home page to have an awesome picture of kelsey and me

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,27 @@ import CardContainer from "../containers/CardContainer";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
-  root: {
-    textAlign: "center",
-    paddingTop: theme.spacing.unit * 20
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
   }
 });
 
 class Index extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
       <CardContainer>
         <Grid container justify="center">
           <Grid item>
             <Card>
+              <CardMedia
+                image={"/static/sedona.jpg"}
+                title="Mt Whitney Summit"
+                className={classes.media}
+              />
               <CardContent>
-                <Typography variant="h6">
+                <Typography variant="subtitle1">
                   I'm a sofware engineer currently located in the Phoenix area.
                   At the moment I am enjoying working with React and Nodejs
                   applications. Some of my current interests are load testing,
@@ -35,10 +41,6 @@ class Index extends React.Component {
                   Also, I like to help people which is why I work at Solera
                   Health.
                 </Typography>
-                <CardMedia
-                  image={"/static/whitney-summit.jpg"}
-                  title="Mt Whitney Summit"
-                />
               </CardContent>
             </Card>
           </Grid>
